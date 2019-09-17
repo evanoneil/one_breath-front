cd9d200e52bfe9aa64ffc4c9784861a75d0d3819
Evan ONeil <evan@evanoneil.net>
2019-09-17 10:41:59 -0500

    new components

--- src/components/partners.js
+++ src/components/partners.js
@@ -2,6 +2,12 @@ import { Link } from "gatsby";
 import React from "react";
 import Icon from "./icon";
 import { cn } from "../lib/helpers";
+import aah from "../../public/assets/img/aah.png";
+import edf from "../../public/assets/img/edf.png";
+import pc from "../../public/assets/img/pc.png";
+import et from "../../public/assets/img/et.jpg";
+import eip from "../../public/assets/img/eip.png";
+import rice from "../../public/assets/img/rice.png";
 
 import styles from "./partners.module.css";
 
@@ -9,17 +15,29 @@ import "../layouts/tachyons/css/tachyons.css";
 import "../layouts/custom.css";
 
 const Partners = () => (
-  <div className="tc">
-    <h2 className="mt0 mb0 gold center">One Breath Partnership is</h2>
-
+  <div className="tc ">
+    <h1 className="mt0 mb0 gold center">One Breath Partnership is</h1>
+    <br />
     <br />
     <div className={styles.displaygrid}>
-      <div>AAH</div>
-      <div>EDF</div>
-      <div>PC</div>
-      <div>ET</div>
-      <div>EIP</div>
-      <div>Rice</div>
+      <div>
+        <img className="w-80" src={aah} />
+      </div>
+      <div>
+        <img src={edf} />
+      </div>
+      <div>
+        <img src={pc} />
+      </div>
+      <div>
+        <img src={et} />
+      </div>
+      <div>
+        <img src={eip} />
+      </div>
+      <div>
+        <img src={rice} />
+      </div>
     </div>
     <a
       className="center pa3 ttu mid-gray f5 fw7 blue no-underline dib-l enterprise mt5"
