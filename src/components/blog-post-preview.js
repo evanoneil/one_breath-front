b453125a394b729cbc3ce36ece132c3718a16b26
Evan ONeil <evan@evanoneil.net>
2019-10-07 16:07:39 -0500

    filed under link issue

--- src/components/blog-post-preview.js
+++ src/components/blog-post-preview.js
@@ -10,50 +10,52 @@ import { responsiveTitle3 } from "./typography.module.css";
 
 function BlogPostPreview(props) {
   return (
-    <Link
-      className={props.isInList ? styles.inList : styles.inGrid}
-      to={getBlogUrl(props.publishedAt, props.slug.current)}
-    >
-      <div className="mb3">
-        {props.categories && (
-          <div>
-            {props.categories.map(category => (
-              <span key={category._id} className={category.color}>
-                {category.title}
-              </span>
-            ))}
-          </div>
-        )}
-      </div>
-      <div className={styles.displaygrid}>
-        <div className={styles.leadMediaThumb}>
-          {props.mainImage && props.mainImage.asset && (
-            <img
-              className={styles.zoom}
-              src={imageUrlFor(buildImageObj(props.mainImage))
-                .width(600)
-                .height(Math.floor((9 / 16) * 600))
-                .auto("format")
-                .url()}
-              alt={props.mainImage.alt}
-            />
+    <div>
+      <Link
+        className={props.isInList ? styles.inList : styles.inGrid}
+        to={getBlogUrl(props.publishedAt, props.slug.current)}
+      >
+        <div className="mb3">
+          {props.categories && (
+            <div>
+              {props.categories.map(category => (
+                <span key={category._id} className={category.color}>
+                  {category.title}
+                </span>
+              ))}
+            </div>
           )}
         </div>
-        <div className={styles.text}>
-          <h3 className={cn(responsiveTitle3, styles.title)}>
-            <a>{props.title}</a>
-          </h3>
+        <div className={styles.displaygrid}>
+          <div className={styles.leadMediaThumb}>
+            {props.mainImage && props.mainImage.asset && (
+              <img
+                className={styles.zoom}
+                src={imageUrlFor(buildImageObj(props.mainImage))
+                  .width(600)
+                  .height(Math.floor((9 / 16) * 600))
+                  .auto("format")
+                  .url()}
+                alt={props.mainImage.alt}
+              />
+            )}
+          </div>
+          <div className={styles.text}>
+            <h3 className={cn(responsiveTitle3, styles.title)}>
+              <a>{props.title}</a>
+            </h3>
 
-          <div className={styles.date}>
-            <h4>
-              By {props.authors[0].author.name}
-              <br />
-              {format(props.publishedAt, "MM.D.YYYY")}
-            </h4>
+            <div className={styles.date}>
+              <h4>
+                By {props.authors[0].author.name}
+                <br />
+                {format(props.publishedAt, "MM.D.YYYY")}
+              </h4>
+            </div>
           </div>
         </div>
-      </div>
-    </Link>
+      </Link>
+    </div>
   );
 }
 
