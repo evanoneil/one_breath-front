55aac750a7d3983f440b476e1f4bb08ff376e271
Evan ONeil <evan@evanoneil.net>
2020-01-29 11:18:37 -0600

    1.29 petition

--- src/components/partner-highlight 2.js
+++ src/components/partner-highlight 2.js
@@ -3,8 +3,6 @@ import React from "react";
 import Icon from "./icon";
 import { cn } from "../lib/helpers";
 import EipHighlight from "../../public/assets/img/eip_highlight.jpg";
-import PetitionHighlight from "../../public/assets/img/petition_highlight.jpg";
-import CoastalBreeze from "../../public/assets/img/coastal_breeze.jpg';
 
 import styles from "./partner-highlight.module.css";
 
@@ -18,20 +16,21 @@ const PartnerHighlight = () => (
     </div>
 
     <div className="pa5-ns pt4 pt3-ns pb0 flex flex-wrap w-50-ns w-100">
-      <h3 className="mt0 mb0 green ttu">ENOUGH IS ENOUGH</h3>
+      <h3 className="mt0 mb0 green ttu">THE THIN GREEN LINE</h3>
       <hr />
       <h2 className="mb0">
-      TCEQ must take its mission to protect our health and safety seriously.      </h2>
+      As Texas' petrochemical industry grows, the environmental agency meant to protect our health and safety is shrinking.
+      </h2>
 
-      {/* <p className="mb5-ns slate-gray">
+      <p className="mb5-ns slate-gray">
       In the last 10 years, the Environmental Integrity Project reports, the Texas Commission on Environmental Quality's budget has been cut by 35% — more than every other state, except one.
-      </p> */}
+      </p>
       <a
         className="pa3 ttu mid-gray f5 fw7 blue no-underline dib-l enterprise"
-        href="/take-action"
+        href="https://www.environmentalintegrity.org/news/state-funding-for-environmental-programs-slashed"
         target="blank"
       >
-        Sign the petition →
+        Read the report
       </a>
     </div>
   </div>
