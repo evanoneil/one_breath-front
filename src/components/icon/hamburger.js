2313e56cb79665cd3243cfa0bee6c0e5d358afc7
Evan ONeil <evan@evanoneil.net>
2020-02-11 15:44:22 -0600

    subscribe OG update

--- src/components/icon/hamburger.js
+++ src/components/icon/hamburger.js
@@ -3,7 +3,18 @@ import React from 'react'
 const strokeStyle = {vectorEffect: 'non-scaling-stroke'}
 
 const HamburgerIcon = () => (
-  <svg height="32px" id="Layer_1" style="enable-background:new 0 0 32 32;" version="1.1" viewBox="0 0 32 32" width="32px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M4,10h24c1.104,0,2-0.896,2-2s-0.896-2-2-2H4C2.896,6,2,6.896,2,8S2.896,10,4,10z M28,14H4c-1.104,0-2,0.896-2,2  s0.896,2,2,2h24c1.104,0,2-0.896,2-2S29.104,14,28,14z M28,22H4c-1.104,0-2,0.896-2,2s0.896,2,2,2h24c1.104,0,2-0.896,2-2  S29.104,22,28,22z"/></svg>
+  <svg
+    viewBox='0 0 25 25'
+    fill='none'
+    xmlns='http://www.w3.org/2000/svg'
+    preserveAspectRatio='xMidYMid'
+    width='1em'
+    height='1em'
+  >
+    <path d='M5 7.5H20' stroke='currentColor' style={strokeStyle} />
+    <path d='M5 12.5H20' stroke='currentColor' style={strokeStyle} />
+    <path d='M5 17.5H20' stroke='currentColor' style={strokeStyle} />
+  </svg>
 )
 
 export default HamburgerIcon
