50369de827c564d4cf4e765a20f507017258a3f4
Evan ONeil <evan@evanoneil.net>
2020-04-20 14:27:40 -0500

    Author Pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -85,3 +85,16 @@ exports.createPages = async ({graphql, actions, reporter}) => {
   await createBlogPostPages(graphql, actions, reporter)
   await createAuthorPages(graphql, actions, reporter)
 }
+
+exports.onCreateWebpackConfig = ({stage, actions, getConfig}) => {
+  if (stage === 'build-javascript') {
+    const config = getConfig()
+    const miniCssExtractPlugin = config.plugins.find(
+      plugin => plugin.constructor.name === 'MiniCssExtractPlugin'
+    )
+    if (miniCssExtractPlugin) {
+      miniCssExtractPlugin.options.ignoreOrder = true
+    }
+    actions.replaceWebpackConfig(config)
+  }
+}
