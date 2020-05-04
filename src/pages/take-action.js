828baf138a3ec870c551153a006f80941aeb40ea
Evan ONeil <evan@evanoneil.net>
2020-05-04 09:54:35 -0500

    new take-action

--- src/pages/take-action.js
+++ src/pages/take-action.js
@@ -1,16 +1,16 @@
-import React from "react";
-import { graphql } from "gatsby";
-import { mapEdgesToNodes } from "../lib/helpers";
-import BlogPostPreviewGrid from "../components/blog-post-preview-grid";
-import NewsroomPreviewGrid from "../components/newsroom-preview-grid";
-import Container from "../components/container";
-import GraphQLErrorList from "../components/graphql-error-list";
-import SEO from "../components/seo";
-import Layout from "../containers/layout";
-import Helmet from "react-helmet";
-import ogImage from "../../public/assets/img/one_breath-petition-OG.jpg";
+import React from 'react'
+import {graphql} from 'gatsby'
+import {mapEdgesToNodes} from '../lib/helpers'
+import BlogPostPreviewGrid from '../components/blog-post-preview-grid'
+import NewsroomPreviewGrid from '../components/newsroom-preview-grid'
+import Container from '../components/container'
+import GraphQLErrorList from '../components/graphql-error-list'
+import SEO from '../components/seo'
+import Layout from '../containers/layout'
+import Helmet from 'react-helmet'
+import ogImage from '../../public/assets/img/one_breath-petition-OG.jpg'
 
-import { responsiveTitle1 } from "../components/typography.module.css";
+import {responsiveTitle1} from '../components/typography.module.css'
 
 // export const query = graphql`
 //   query NewsroomPageQuery {
@@ -52,45 +52,53 @@ import { responsiveTitle1 } from "../components/typography.module.css";
 // `;
 
 const TakeActionPage = props => {
-  const { data, errors } = props;
+  const {data, errors} = props
 
   if (errors) {
     return (
       <Layout>
         <GraphQLErrorList errors={errors} />
       </Layout>
-    );
+    )
   }
 
-  const postNodes = data && data.posts && mapEdgesToNodes(data.posts);
-  const mediaNodes = data && data.media && mapEdgesToNodes(data.media);
-  const description = "Living every day like an ozone action day.";
-  const image = `https://onebreathhou.org${ogImage}`;
+  const postNodes = data && data.posts && mapEdgesToNodes(data.posts)
+  const mediaNodes = data && data.media && mapEdgesToNodes(data.media)
+  const description = 'Living every day like an ozone action day.'
+  const image = `https://onebreathhou.org${ogImage}`
 
   return (
     <Layout>
       {/* <SEO title="Take Action" /> */}
       <Helmet>
         <title>Take Action | One Breath Partnership</title>
-        <meta property="og:title" content="Take Action | One Breath Partnership" />
-        <meta name="description" content={description} />
-        <meta name="image" content={image} />
-        <meta name="og:image" content={image} />
-
-        <meta name="twitter:title" content="Take Action | One Breath Partnership" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:creator" content="@onebreathhou" />
-        <meta name="twitter:description" content={description} />
-        <meta name="twitter:image" content={image} />
-
-        <link href='https://actionnetwork.org/css/style-embed-v3.css' rel='stylesheet' type='text/css' /><script src='https://actionnetwork.org/widgets/v3/petition/the-texas-commission-on-environmental-quality-needs-to-get-serious?format=js&source=widget&style=full'></script>      </Helmet>
+        <meta property='og:title' content='Take Action | One Breath Partnership' />
+        <meta name='description' content={description} />
+        <meta name='image' content={image} />
+        <meta name='og:image' content={image} />
+        <meta name='twitter:title' content='Take Action | One Breath Partnership' />
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta name='twitter:creator' content='@onebreathhou' />
+        <meta name='twitter:description' content={description} />
+        <meta name='twitter:image' content={image} />
+        <link
+          href='https://actionnetwork.org/css/style-embed-v3.css'
+          rel='stylesheet'
+          type='text/css'
+        />
+        <script src='https://actionnetwork.org/widgets/v3/petition/houston-needs-more-local-air-monitoring-to-protect-our-health?format=js&source=widget' />{' '}
+      </Helmet>
 
       <Container>
-      <div id='can-petition-area-the-texas-commission-on-environmental-quality-needs-to-get-serious' ></div>
-
+        <div
+          id='can-petition-area-houston-needs-more-local-air-monitoring-to-protect-our-health'
+          style='width: 600px'
+        >
+          {/* !-- this div is the target for our HTML insertion --> */}
+        </div>
       </Container>
     </Layout>
-  );
-};
+  )
+}
 
-export default TakeActionPage;
+export default TakeActionPage
