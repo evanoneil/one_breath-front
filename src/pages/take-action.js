3324155fc178f519c11db9da981f28e64f7e5376
Evan ONeil <evan@evanoneil.net>
2020-05-15 11:35:50 -0500

    new take action page

--- src/pages/take-action.js
+++ src/pages/take-action.js
@@ -97,19 +97,16 @@ const TakeActionPage = props => {
       <Container>
         <div>
           <h1 className='w-80 orange'>
-            Tell TCEQ: Houston needs more local air monitoring to protect our health
+            Tell the EPA to protect our shared home from particle pollution{' '}
           </h1>
           <div className='w-100 bg-orange  flex flex-wrap'>
             <div className='w-50-ns w-100 f3 pa4 black fw7  flex-wrap'>
               <p className='fw6 f6 ttu mb0'>action item:</p>
-              <h3 className='mt0'>E-mail TCEQ by May 14th</h3>
+              <h3 className='mt0'>SUBMIT A COMMENT TO THE EPA</h3>
               <a
                 className='pa3 ttu b--yellow b--solid bw2 f4 fw7 black br5 no-underline dib-l  mt1'
-                href='mailto:monops@tceq.texas.gov?bcc=stories@onebreathhou.org&subject=2020 Annual Monitoring Network Plan&body=I am writing today strongly in favor of installing a new PM 2.5 monitor in west Houston. While this is a crucial first step toward addressing pollution and protecting the health of the people of Houston, much more action is needed.
-
-All Houstonians face toxic air pollution, and it is costing us dearly — in more ways than one. According to new analysis from the Harvard School of Public Health and Environmental Defense Fund, widespread exposure to PM 2.5 air pollution in 2015 across Houston and surrounding neighborhoods was responsible for more than 5,000 premature deaths, and nearly $50 billion in economic damages.
-
-Please finalize the implementation of this PM 2.5 monitor and immediately take steps to install additional community monitors. Our city desperately needs this data and the state to act on it to reduce air pollution and its harmful health impacts.'
+                href='https://www.regulations.gov/comment?D=EPA-HQ-OAR-2015-0072-0069'
+                target='_blank'
               >
                 Take Action →
               </a>
@@ -117,14 +114,14 @@ Please finalize the implementation of this PM 2.5 monitor and immediately take s
             <div className='w-50-ns w-100   flex-wrap'>
               <img
                 className='w-100'
-                src='https://can2-prod.s3.amazonaws.com/petitions/photos/000/172/441/original/pm-map.jpg'
+                src='https://can2-prod.s3.amazonaws.com/petitions/photos/000/174/978/original/girl-with-inhaler-blur.jpg'
               />
             </div>
           </div>
 
           <p className='caption'>
-            Almost all of Houston is exposed to fine particulate matter above the level that health
-            scientists say should be the new EPA standard.
+            According to the American Lung Association, nearly 100,000 children in Houston and
+            Harris County suffer from asthma.
           </p>
           <br />
           {/* <a
@@ -141,91 +138,87 @@ Please finalize the implementation of this PM 2.5 monitor and immediately take s
           <br /> */}
           <div className='w-75'>
             <p>
-              We have a right to breathe clean air, and we have a right to know what we are
-              breathing.
+              The science is clear. It’s consistent. Exposure to{' '}
+              <a
+                href='https://onebreathhou.org/newsroom/2020/04/particle-pollution-soot-standards-epa-houston/'
+                target='_blank'
+              >
+                tiny airborne particles
+              </a>{' '}
+              in the air sends too many Americans to the hospital and the emergency room. It leads
+              to too many premature deaths from lung disease, heart attacks, strokes, asthma and
+              cancer.
             </p>
             <p>
-              Right now, almost all of Houston is exposed to&nbsp;
-              <b>
-                <a
-                  className='orange'
-                  href='https://onebreathhou.org/newsroom/2020/04/particle-pollution-soot-standards-epa-houston/'
-                  target='_blank'
-                >
-                  fine particulate matter
-                </a>
-                &nbsp;
-              </b>
-              above the level that health scientists say should be the new, stronger federal
-              standard. The Texas Commission on Environmental Quality, or TCEQ, has proposed placing
-              a <b>new tool</b> that can monitor this kind of pollution in west Houston. That is
-              just a first step toward addressing air pollution and protecting our health, but it's
-              one&nbsp;
-              <b>
-                <a
-                  className='orange'
-                  href='https://www.tceq.texas.gov/airquality/monops/network_review.html'
-                  target='_blank'
-                >
-                  we need to tell them to take
-                </a>
-                .
-              </b>
+              These polluting particles, also known as soot, spew from power plants, cars and trucks
+              and industrial facilities. They penetrate our lungs and cause some of the same
+              diseases that are making COVID-19 more deadly. In Houston, widespread exposure to
+              particle pollution contributed to <strong>more than 5,000 premature deaths</strong>{' '}
+              and <strong>nearly $50 billion in economic damages</strong> in 2015 alone, a new
+              analysis by the Harvard School of Public Health and Environmental Defense Fund found.
             </p>
             <p>
-              Fine particulate matter, sometimes called PM 2.5 or soot, knows no boundaries. As the
-              Environmental Protection Agency is moving to maintain a&nbsp;
-              <b>
-                <a
-                  className='orange'
-                  href='https://onebreathhou.org/newsroom/2020/04/particulate-matter-standards-epa-science-public-health/'
-                  target='_blank'
-                >
-                  weaker federal standard
-                </a>
-              </b>
-              , we need <b>strong local data</b>. These invisible airborne particles, which escape
-              from power plants, cars and trucks and industrial facilities, can lodge in our lungs,
-              enter our bloodstream and cause heart attacks, strokes and lung cancer. Exposure to
-              these particles is <b>the largest environmental risk</b> for early death in the U.S.
-              In Houston, particle pollution contributed to <b>more than 5,000 early deaths</b> and
-              nearly <b>$50 billion in economic damages</b> in 2015 alone, a new analysis from the
-              Harvard School of Public Health and Environmental Defense Fund finds. We need to make
-              sure that TCEQ follows through on their commitment to place a new monitor in a part of
-              the city that lacks one — and encourage them to take further action to keep us safe.
-              As the coronavirus pandemic has demonstrated, we need to have access to the best
-              information, so we can make the best decisions. A new monitor for this kind of air
-              pollution is the best decision for our shared home.
+              That's why the Environmental Protection Agency needs to strengthen the federal{' '}
+              <a
+                className='bold'
+                href='https://onebreathhou.org/newsroom/2020/04/particulate-matter-standards-epa-science-public-health/'
+                target='_blank'
+              >
+                standard
+              </a>
+              .
             </p>
             <p>
-              As the coronavirus pandemic has demonstrated, we need to have access to the best
-              information, so we can make the best decisions. A new monitor for this kind of air
-              pollution is the best decision for our shared home.&nbsp;
-              <b>
-                Click the button below to send an email with an automated message or use the
-                information here to explain in your own words why we need this monitor now.
-              </b>
+              This spring, the agency’s own scientists found that <strong>strengthening </strong>it
+              could save <strong>more than 10,000 American lives</strong> every year — but agency
+              leadership didn’t listen. Former coal lobbyist Andrew Wheeler, appointed by President
+              Trump to head the EPA, <strong>ignored the science</strong> and decided to keep{' '}
+              <a
+                href='https://www.nytimes.com/2020/04/14/climate/coronavirus-soot-clean-air-regulations.html'
+                target='_blank'
+              >
+                the outdated and inadequate standard
+              </a>
+              .{' '}
+            </p>
+            <p>
+              So, let’s be clear: This is the wrong decision.
+              <br />
+            </p>
+            <p>
+              Together, <strong>we have the power</strong> to save 10,000 lives.
+            </p>
+            <p>
+              Wheeler’s decision opens up a 60-day period of public comments.{' '}
+              <a
+                href='https://www.regulations.gov/comment?D=EPA-HQ-OAR-2015-0072-0069'
+                target='_blank'
+              >
+                They need to hear from us
+              </a>
+              . Together, let’s make it clear that the EPA needs to listen to the science and
+              protect our shared home from this deadly pollution.
+            </p>
+            <p>
+              That’s <strong>the right decision</strong>.
             </p>
           </div>
-        </div>
-        <div className='w-100 bg-orange mb3 flex flex-wrap v-mid'>
-          <div className='w-50-ns w-100 f3 pa4 black fw7  flex-wrap v-mid'>
-            <p className='fw6 f6 ttu mb0'>action item:</p>
-            <h3 className='mt0'>E-mail TCEQ by May 14th</h3>
-            <a
-              className='pa3 ttu b--yellow b--solid bw2 f4 fw7 black br5 no-underline dib-l  mt1'
-              href='mailto:monops@tceq.texas.gov?bcc=stories@onebreathhou.org&subject=2020 Annual Monitoring Network Plan&body=I am writing today strongly in favor of installing a new PM 2.5 monitor in west Houston. While this is a crucial first step toward addressing pollution and protecting the health of the people of Houston, much more action is needed.
-
-All Houstonians face toxic air pollution, and it is costing us dearly — in more ways than one. According to new analysis from the Harvard School of Public Health and Environmental Defense Fund, widespread exposure to PM 2.5 air pollution in 2015 across Houston and surrounding neighborhoods was responsible for more than 5,000 premature deaths, and nearly $50 billion in economic damages.
-
-Please finalize the implementation of this PM 2.5 monitor and immediately take steps to install additional community monitors. Our city desperately needs this data and the state to act on it to reduce air pollution and its harmful health impacts.'
-            >
-              E-mail TCEQ →
-            </a>
-            <br />
-            <br />
+          <div className='w-100 bg-orange mb3 flex flex-wrap v-mid'>
+            <div className='w-50-ns w-100 f3 pa4 black fw7  flex-wrap v-mid'>
+              <p className='fw6 f6 ttu mb0'>action item:</p>
+              <h3 className='mt0'>SUBMIT A COMMENT TO THE EPA</h3>
+              <a
+                className='pa3 ttu b--yellow b--solid bw2 f4 fw7 black br5 no-underline dib-l  mt1'
+                href='https://www.regulations.gov/comment?D=EPA-HQ-OAR-2015-0072-0069'
+                target='_blank'
+              >
+                TAKE ACTION →
+              </a>
+              <br />
+              <br />
+            </div>
+            <div className='w-50-ns w-100 pa4 mt2 flex-wrap v-mid' />
           </div>
-          <div className='w-50-ns w-100 pa4 mt2 flex-wrap v-mid' />
         </div>
       </Container>
     </Layout>
