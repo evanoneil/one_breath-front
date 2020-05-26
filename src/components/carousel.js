e63b2940cf60bc20ebf7c9392f0a7763e817f34f
Evan ONeil <evan@evanoneil.net>
2020-05-25 22:08:49 -0500

    newsroom slider fix

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -19,39 +19,40 @@ const Carousel = props => (
         rel='stylesheet'
         href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.3.1/css/bootstrap.min.css'
       /> */}
-      <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js' />
-      <script src='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.3.1/js/bootstrap.min.js' />
+      <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js' />
+      <script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js' />
+      <script src='https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js' />{' '}
       <script src='./script.js' />
     </Helmet>
     <h2 className='orange ml6'>In the Media</h2>{' '}
-    <div className='carousel slide' id='myCarousel'>
+    <div className='carousel slide' id='carousel'>
       <div className='carousel-inner'>
-        <div className='item active'>
+        <div className='carousel-item active'>
           {props.mediaNodes && <NewsroomPreviewGrid nodes={props.mediaNodes} />}
         </div>
 
-        <div className='item'>
+        <div className='carousel-item'>
           {props.mediaNodes2 && <NewsroomPreviewGrid nodes={props.mediaNodes2} />}
         </div>
 
-        <div className='item'>
+        <div className='carousel-item'>
           {props.mediaNodes3 && <NewsroomPreviewGrid nodes={props.mediaNodes3} />}
         </div>
 
-        <div className='item'>
+        <div className='carousel-item'>
           {props.mediaNodes4 && <NewsroomPreviewGrid nodes={props.mediaNodes4} />}
         </div>
 
-        <div className='item'>
+        <div className='carousel-item'>
           {props.mediaNodes5 && <NewsroomPreviewGrid nodes={props.mediaNodes5} />}
         </div>
       </div>
 
       <div className='control-box'>
-        <a data-slide='prev' href='#myCarousel' className='carousel-control left'>
+        <a data-slide='prev' href='#carousel' className='carousel-control left'>
           ‹
         </a>
-        <a data-slide='next' href='#myCarousel' className='carousel-control right'>
+        <a data-slide='next' href='#carousel' className='carousel-control right'>
           ›
         </a>
       </div>
