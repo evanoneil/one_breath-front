e8af59311acf84a1dda2f12d0e9a3a5c06821dd5
Evan ONeil <evan@evanoneil.net>
2020-09-17 09:18:05 -0700

    blog template fix

--- src/components/blog-post-new.js
+++ src/components/blog-post-new.js
@@ -1,7 +1,7 @@
-import {format, distanceInWords, differenceInDays} from 'date-fns'
+import { format, distanceInWords, differenceInDays } from 'date-fns'
 import React from 'react'
-import {buildImageObj, filterOutDocsWithoutSlugs} from '../lib/helpers'
-import {imageUrlFor} from '../lib/image-url'
+import { buildImageObj, filterOutDocsWithoutSlugs } from '../lib/helpers'
+import { imageUrlFor } from '../lib/image-url'
 import PortableText from './portableText'
 import Container from './container'
 import AuthorList from './author-list'
@@ -10,21 +10,21 @@ import Helmet from 'react-helmet'
 import getYouTubeId from 'get-youtube-id'
 import YouTube from 'react-youtube'
 
-import {FaTwitter, FaFacebook, FaEnvelope} from 'react-icons/fa'
+import { FaTwitter, FaFacebook, FaEnvelope } from 'react-icons/fa'
 
 import styles from './blog-post.module.css'
 
 const serializers = {
   types: {
-    youtube: ({node}) => {
-      const {url} = node
+    youtube: ({ node }) => {
+      const { url } = node
       const id = getYouTubeId(url)
       return <YouTube videoId={id} />
     }
   }
 }
 
-function BlogPost (props) {
+function BlogPost(props) {
   const {
     _rawBody,
     blocks,
@@ -51,30 +51,36 @@ function BlogPost (props) {
   const emailShare = `mailto:?subject=I%20just%20read%20this%20great%20article%20on%20One%20Breath%20Partnership's%20Website!&body=${shareUrl}`
   return (
     <article className={styles.root}>
+
       {mainImage && mainImage.asset && (
-        <div className={styles.mainImage}>
-          <img
-            src={imageUrlFor(buildImageObj(mainImage))
-              .width(1920)
-              .height(Math.floor((9 / 16) * 1200))
-              .fit('crop')
-              .auto('format')
-              .url()}
-            alt={mainImage.alt}
-          />
-          <div className={styles.example}>
-            <div>
-              <span>
-                {categories.map(category => (
-                  <a className={styles.pill} key={category._id}>
-                    {category.title}
-                  </a>
-                ))}
-              </span>
-              <div className={styles.publishedAt}> </div>
-            </div>
+        <div class="w-100 pa5 flex">
+          <div class="w-50 ">
+            <img
+              src={imageUrlFor(buildImageObj(mainImage))
+                .width(960)
+                .fit('crop')
+                .auto('format')
+                .url()}
+              alt={mainImage.alt}
+            />
+          </div>
+
+          <div class="w-50">
+
             <h1 className={styles.title}>{title}</h1>
-            <div>
+
+          </div>
+
+        </div>
+
+      )}          <p className={styles.caption}>{mainImage.caption}</p>
+
+
+      <Container>
+        <div>
+          <div class="w-100 flex">
+            <div className="w-30">
+              {authors && <AuthorList items={authors} />}
               {' '}
               {publishedAt && (
                 <div className={styles.publishedAt}>
@@ -83,22 +89,34 @@ function BlogPost (props) {
                     : format(new Date(publishedAt), 'MMMM Do, YYYY')}
                 </div>
               )}
+              <div>
+                <span>
+                  {categories.map(category => (
+                    <a class="dib" key={category._id}>
+                      {category.title}
+                    </a>
+
+                  ))}
+                </span>
+                <div className={styles.publishedAt}> </div>
+                <div class="fixed">
+                  <hr className='mt5 bw1 b--solid' />
+
+                  <h4 className='ttu'>Share this article</h4>
+                  <a href={twitterShare} target='blank'>
+                    <FaTwitter className='f2 mr2 ml2' />
+                  </a>
+                  <a href={fbShare} target='blank'>
+                    <FaFacebook className='f2 mr2 ml2' />
+                  </a>
+                  <a href={emailShare} target='blank'>
+                    <FaEnvelope className='f2 mr2 ml2' />
+                  </a>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      )}
-      <Container>
-        <div>
-          <Helmet>
-            <link rel='stylesheet' href='https://unpkg.com/aos@2.3.0/dist/aos.css' />
-            <script src='https://unpkg.com/aos@2.3.0/dist/aos.js' />
-            <script src='https://wp.evanoneil.net/one_breath/script.js' />
-          </Helmet>
-          <p className={styles.caption}>{mainImage.caption}</p>
-          <div className={styles.grid}>
-            <div />
-            <div className={styles.mainContent}>
-              {authors && <AuthorList items={authors} />}
+
+            <div class="w-70 ">
 
               {_rawBody && <PortableText blocks={_rawBody} serializers={serializers} />}
               <div className={styles.takeaction}>
@@ -121,29 +139,16 @@ function BlogPost (props) {
                   </li>
                 </ol>
               </div>
-              <div>
-                <hr className='mt5 bw1 b--solid' />
-
-                <h4 className='ttu'>Share this article</h4>
-                <a href={twitterShare} target='blank'>
-                  <FaTwitter className='f2 mr2 ml2' />
-                </a>
-                <a href={fbShare} target='blank'>
-                  <FaFacebook className='f2 mr2 ml2' />
-                </a>
-                <a href={emailShare} target='blank'>
-                  <FaEnvelope className='f2 mr2 ml2' />
-                </a>
-                {/* <hr className="mt4" /> */}
-              </div>
             </div>
-            {/* <aside className={styles.metaContent}></aside> */}
+
           </div>
+
+          {/* <aside className={styles.metaContent}></aside> */}
         </div>
         <br />
         <SignUp />
-      </Container>
-    </article>
+      </Container >
+    </article >
   )
 }
 
