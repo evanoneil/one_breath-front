d15533909c779f0a37705db2fb69216d6ee67dc7
Evan O'Neil <evan@evanoneil.net>
2021-08-13 09:50:55 -0500

    Add files via upload

---
+++ src/components/arrow.js
@@ -0,0 +1,36 @@
+import { Link } from "gatsby";
+import React from "react";
+import Icon from "./icon";
+import { cn } from "../lib/helpers";
+import Helmet from "react-helmet";
+
+import styles from "./arrow.module.css";
+
+import "../layouts/tachyons/css/tachyons.css";
+import "../layouts/custom.css";
+
+const ArrowHighlight = () => (
+  <div className="flex flex-wrap mt6 mb6-ns">
+
+    <div className="pa5-ns pt4 pt3-ns pb0 flex flex-wrap w-50-ns w-100">
+      <h3 className="mt0 mb0 gold">THE HOUSTON ARROW →</h3>
+      <hr />
+      <h1 className="mb0">
+      What "The Houston Arrow’ reveals about the connections between government, development and the concentration of opportunity. A four-part series by Leah Binkovitz.
+            </h1>
+
+      <br />
+      <a
+        className="pa3 ttu mid-gray f5 fw7 blue no-underline dib-l enterprise mt5"
+        href="/houston-arrow"
+      >
+       
+        READ
+      </a>
+    </div>
+    <div className={styles.lefthighlight}>
+      <div className={styles.interiorcolor}></div>
+    </div>
+  </div>
+);
+export default ArrowHighlight;
