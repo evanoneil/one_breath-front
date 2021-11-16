fd522ee4316d0e0ccaebe9a4b5c74632842f437e
Piyush Kalsariya <piyushkalasariya555@gmail.com>
2021-11-16 18:21:44 +0530

    Algolia Mobile view updates

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -91,50 +91,54 @@ const Searchbar = props => {
           <path d='M16.041 15.856c-0.034 0.026-0.067 0.055-0.099 0.087s-0.060 0.064-0.087 0.099c-1.258 1.213-2.969 1.958-4.855 1.958-1.933 0-3.682-0.782-4.95-2.050s-2.050-3.017-2.050-4.95 0.782-3.682 2.050-4.95 3.017-2.050 4.95-2.050 3.682 0.782 4.95 2.050 2.050 3.017 2.050 4.95c0 1.886-0.745 3.597-1.959 4.856zM21.707 20.293l-3.675-3.675c1.231-1.54 1.968-3.493 1.968-5.618 0-2.485-1.008-4.736-2.636-6.364s-3.879-2.636-6.364-2.636-4.736 1.008-6.364 2.636-2.636 3.879-2.636 6.364 1.008 4.736 2.636 6.364 3.879 2.636 6.364 2.636c2.125 0 4.078-0.737 5.618-1.968l3.675 3.675c0.391 0.391 1.024 0.391 1.414 0s0.391-1.024 0-1.414z' />
         </svg>
       </div>
-      <div className={classes.ResultWrapper} ref={wrapperRef}>
-        <ul>
-          {searchResult.map((news, newsIndex) => {
-            return (
-              <li
-                key={`data-${newsIndex}`}
-                onClick={() => {
-                  window.open(news.url, '_blank')
-                  setSearchResult([])
-                }}
-              >
-                <div style={{display: 'flex'}}>
-                  <div
-                    style={{
-                      backgroundImage: `url(${news.image})`,
-                      height: 45,
-                      minWidth: 60,
-                      maxWidth: 60,
-                      backgroundSize: 'contain',
-                      backgroundColor: '#ddd'
-                    }}
-                  >
-                    {/* <img src={news.image} alt='news' /> */}
+      {searchResult.length > 0 && (
+        <div className={classes.ResultWrapper} ref={wrapperRef}>
+          <ul>
+            {searchResult.map((news, newsIndex) => {
+              return (
+                <li
+                  key={`data-${newsIndex}`}
+                  onClick={() => {
+                    window.open(news.url, '_blank')
+                    setSearchResult([])
+                  }}
+                >
+                  <div style={{display: 'flex'}}>
+                    <div
+                      style={{
+                        backgroundImage: `url(${news.image})`,
+                        height: 45,
+                        minWidth: 60,
+                        maxWidth: 60,
+                        backgroundSize: 'contain',
+                        backgroundColor: '#ddd'
+                      }}
+                    >
+                      {/* <img src={news.image} alt='news' /> */}
+                    </div>
+                    <div className={classes.content}>
+                      {news?._highlightResult?.title?.value && (
+                        <p
+                          className={classes.title}
+                          dangerouslySetInnerHTML={{__html: news._highlightResult.title.value}}
+                        />
+                      )}
+                      {news?._highlightResult?.description?.value && (
+                        <p
+                          className={classes.desc}
+                          dangerouslySetInnerHTML={{
+                            __html: news._highlightResult.description.value
+                          }}
+                        />
+                      )}
+                    </div>
                   </div>
-                  <div className={classes.content}>
-                    {news?._highlightResult?.title?.value && (
-                      <p
-                        className={classes.title}
-                        dangerouslySetInnerHTML={{__html: news._highlightResult.title.value}}
-                      />
-                    )}
-                    {news?._highlightResult?.description?.value && (
-                      <p
-                        className={classes.desc}
-                        dangerouslySetInnerHTML={{__html: news._highlightResult.description.value}}
-                      />
-                    )}
-                  </div>
-                </div>
-              </li>
-            )
-          })}
-        </ul>
-      </div>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      )}
     </div>
   )
 }
