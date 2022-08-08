f1979f8c167dfb51ee43f5843e00ab529cf6c5c8
Evan O'Neil <evan@evanoneil.net>
2022-08-08 14:51:34 -0500

    Add files via upload

---
+++ src/pages/5-years-after.jsx
@@ -0,0 +1,95 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import { mapEdgesToNodes } from '../lib/helpers'
+import BlogPostPreviewGrid from '../components/blog-post-preview-grid'
+import NewsroomPreviewGrid from '../components/newsroom-preview-grid'
+import Container from '../components/container'
+import GraphQLErrorList from '../components/graphql-error-list'
+import SEO from '../components/seo'
+import Layout from '../containers/layout'
+import Helmet from 'react-helmet'
+import ogImage from '../assets/img/5yrs-lessunreadable-basic_info.png'
+
+import { responsiveTitle1 } from '../components/typography.module.css'
+
+// export const query = graphql`
+//   query NewsroomPageQuery {
+//     posts: allSanityPost(limit: 12, sort: { fields: [publishedAt], order: DESC }) {
+//       edges {
+//         node {
+//           id
+//           categories {
+//             title
+//           }
+//           publishedAt
+//           mainImage {
+//             ...SanityImage
+//             alt
+//           }
+//           title
+//           _rawExcerpt
+//           slug {
+//             current
+//           }
+//         }
+//       }
+//     }
+
+//     media: allSanityNewsroom {
+//       edges {
+//         node {
+//           categories {
+//             title
+//           }
+//           publishedAt
+//           url
+//           outlet
+//           title
+//         }
+//       }
+//     }
+//   }
+// `;
+
+const fiveyrs = props => {
+  const { data, errors } = props
+
+  if (errors) {
+    return (
+      <Layout>
+        <GraphQLErrorList errors={errors} />
+      </Layout>
+    )
+  }
+
+  const postNodes = data && data.posts && mapEdgesToNodes(data.posts)
+  const mediaNodes = data && data.media && mapEdgesToNodes(data.media)
+  const description = 'Air Alliance Houston, the Houston Climate Justice Museum and One Breath invite you to come together to reaffirm community and reflect on the ways Hurricane Harvey changed our city forever.'
+  const image = `https://onebreathhou.org${ogImage}`
+
+  return (
+    <Layout>
+
+    <Helmet>
+      <meta httpEquiv="Refresh" content="0; url='https://secure.airalliancehouston.org/a/5-years-after-landfall'" />
+
+    </Helmet>
+    <Container>
+
+    <div>
+    <h1>Please wait for a redirect to EveryAction</h1>
+  </div>
+  </Container>
+
+    
+    </Layout>
+  )
+}
+
+export default fiveyrs
+
+
+
+
+
+    
