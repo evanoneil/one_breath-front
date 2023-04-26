dcc8a797d49f81137389a1c135db2e9b212ba8f0
Evan O'Neil <evan@evanoneil.studio>
2023-04-26 10:31:41 -0500

    Update itc-deer-park.js

--- src/pages/take-action/itc-deer-park.js
+++ src/pages/take-action/itc-deer-park.js
@@ -1,17 +1,17 @@
 
 import React from 'react'
 import { graphql } from 'gatsby'
-import { mapEdgesToNodes } from '../lib/helpers'
-import BlogPostPreviewGrid from '../components/blog-post-preview-grid'
-import NewsroomPreviewGrid from '../components/newsroom-preview-grid'
-import Container from '../components/container'
-import GraphQLErrorList from '../components/graphql-error-list'
-import SEO from '../components/seo'
-import Layout from '../containers/layout'
+import { mapEdgesToNodes } from '../../lib/helpers'
+import BlogPostPreviewGrid from '../../components/blog-post-preview-grid'
+import NewsroomPreviewGrid from '../../components/newsroom-preview-grid'
+import Container from '../../components/container'
+import GraphQLErrorList from '../../components/graphql-error-list'
+import SEO from '../../components/seo'
+import Layout from '../../containers/layout'
 import Helmet from 'react-helmet'
 import ogImage from '../../assets/img/one_breath-petition-april2023.jpg'
 
-import { responsiveTitle1 } from '../components/typography.module.css'
+import { responsiveTitle1 } from '../../components/typography.module.css'
 
 // export const query = graphql`
 //   query NewsroomPageQuery {
